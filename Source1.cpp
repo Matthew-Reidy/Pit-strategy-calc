721fc7b0de21d608fbee51c936d8e1b030651d5a
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-08-30 12:50:39 -0700

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,15 +1,28 @@
-class MyClass
-{
+#include <vector>
+class MyClass{
 public:
-	double EstFuelCons; //Estimated fuel fonsumption
-	double TTF; //Time til race finish
+	float EstFuelCons; //Estimated fuel fonsumption
+	float TTF; //Time til race finish
 	int FIC; //Current fuel in car
-
-	void getKPL() {   //get kilometers per litre
+	std::vector<float> Laptimes; 
+	
+	void LapToSeconds(int minuit, float seconds) { //inputs Minuit(whole integer), seconds.tht (float number) puts results into vector array
+		float result = 0;
+		
+		minuit =* 60;
+		result = minuit + seconds; 
 
 	}
+	void AvgCalc(std::vector<float> Laptimes, ) { //input previous 5 laps from vector
+						     //median avg formula: sum of all lap times / number of laps 
+		float sumOfAll = 0;
+		for (i = 0, i <= Laptimes.size(); i++) {
+			sumOfAll =+ Laptimes[i];
+		}
+		return sumOfAll / Laptimes.size;
+	}
 	
-	void lapsTillClose() {
+	void ProjectedLaps() {
 
 	}
 	
@@ -17,7 +30,7 @@ public:
 
 	}
 
-	nums(double EstFuelCons, double TTF, int FIC ) {
+	nums(float EstFuelCons, float TTF, int FIC ) {
 		EstimatedFuelConsumption = EstFuelCons;
 		TimeTilFinish = TTF;
 		FuelInCar = FIC;
