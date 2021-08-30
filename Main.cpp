e88d693fd2580a19a247d4634c14fbb6eed968f0
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-08-30 13:33:27 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,18 @@
 // ACCpitstopcalc.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 #include "Header.h"
 #include <iostream>
-
+//place holders in main until I can make gui in c#
 int main()
 {
-   
+	int minuit;
+	float seconds;
+	std::cout << "enter laptime minuit:" << std::endl;
+	std::cin << minuit;
+	std::cout << "enter seconds after the minuit (seconds.xxx)" << std::endl;
+	std::cin << seconds;
+
 }
 
+// Run program: Ctrl + F5 or Debug > Start Withou
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
