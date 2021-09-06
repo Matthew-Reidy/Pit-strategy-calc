b85c077190fe5909316acb8b0cd51d9022d6e7f1
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-09-06 13:34:00 -0700

    Update and rename Source1.cpp to Fuel.cpp

---
+++ Fuel.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <math.h>
+class FuelCalc{
+public:
+	float EstFuelCons; //Estimated fuel fonsumption
+	float TTF; //Time til race finish
+	int FIC; //Current fuel in car
+	std::vector<float> Laptime; 
+	
+	void LapToSeconds(int minuit, float seconds, std::vector<float> Laptimes) { 
+		float result = 0;
+		while (Laptime.size() <= 5) {
+			minuit =* 60;
+			result = minuit + seconds;
+			    continue;
+			if (minuit < 1) {
+				break;
+			}
+		}
+	}
+	int AvgCalc(std::vector<float> Laptimes) { 
+		float sumOfAll = 0;
+		for (i = 0, i <= Laptimes.size; i++) {
+			sumOfAll =+ Laptimes[i];
+			sumOfAll =/ 5;
+		}
+		return sumOfAll;
+	}
+	
+	void ProjectedLaps(int numLaps) {
+		int numLaps = 0;
+		numLaps = TTF / AvgCalc;
+
+	}
+	int FuelForStint(int numLaps) {
+		int LitersReq = 0;
+		litersReq = numLaps * EstFuelCons;
+	}
+	
+
+	FuelCalc(float EstFuelCons, float TTF, int FIC ) {
+		EstimatedFuelConsumption = EstFuelCons;
+		TimeTilFinish = TTF;
+		FuelInCar = FIC;
+	}
+	
+};
+
