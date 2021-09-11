ab6b2fa1bd7da1dbb24cd0f69d9fa8b5d5a09856
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-09-11 12:58:42 -0700

    Update FuelCalc.cs

--- FuelCalc.cs
+++ FuelCalc.cs
@@ -13,40 +13,34 @@ namespace FuelCalculatorDLL
 		static System.Collections.Generic.List<string> LaptimesString = new System.Collections.Generic.List<string>();
 		static List<int> LapTimesNumber = new List<int>();
 
-		static int FinalCalculation()
-		{
+		static int FinalCalculation(){
 			return 0;
 		}
-		static void LapToSeconds(List<string> LaptimesString)
-		{
+		static void LapToSeconds(List<string> LaptimesString){
 			List<int> result = new List<int>();
-			while (LaptimesString.Count <= 5)
-			{
-				for (int i = 0; i <= LaptimesString.Count; i++)
-                {
+			while (LaptimesString.Count <= 5){
+				for (int i = 0; i <= LaptimesString.Count; i++){
 					string[] SplitLapTime;
 					SplitLapTime = LaptimesString[i].Split(':');
 					for (int h = 0; h < SplitLapTime.Length; h++)
                     {
-                        if (int.TryParse(SplitLapTime[h], out int j))
-                        {
+                        if (int.TryParse(SplitLapTime[h], out int j)){
                             result[i] = j * 60;
                         }
                     }
-					if(int.TryParse(SplitLapTime[SplitLapTime.Count()-1], out int k))
-                    {
-						result[i] = result[i] + 60;
+			if(int.TryParse(SplitLapTime[SplitLapTime.Count()-1], out int k)){
+						result[i] += 60;
                     }
                 }
 			}
 			LapTimesNumber = result;
 		}
-		static double AvgCalc()
-		{
+		static double AvgCalc(){
+			
 			double sumOfAll = 0;
 			double average = 0;
-			for (int i = 0; i <= LapTimesNumber.Count; i++)
-			{
+			
+			for (int i = 0; i <= LapTimesNumber.Count; i++){
 				sumOfAll += LapTimesNumber[i];
 
 			}
@@ -55,23 +49,22 @@ namespace FuelCalculatorDLL
 			return average;
 		}
 
-		static int ProjectedLaps()
-		{
+		static int ProjectedLaps(){
+			
 			int numLaps = 0;
 			numLaps = (int)Math.Round(TTF / AvgCalc(),0);
 			return numLaps;
 		}
 
-		static int FuelForStint(int numLaps)
-		{
+		static int FuelForStint(int numLaps){
+			
 			int LitersReq = 0;
 			LitersReq = numLaps * (int)Math.Round(EstFuelCons, 0);
 			return LitersReq;
 		}
 
 
-		static void nums(float EstFuelCons, float TTF, int FIC)
-		{
+		static void nums(float EstFuelCons, float TTF, int FIC){
 			FuelCalc.EstFuelCons = EstFuelCons;
 			FuelCalc.TTF = TTF;
 			FuelCalc.FIC = FIC;
