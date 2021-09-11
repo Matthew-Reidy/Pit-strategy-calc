6d34eff3012785c998cafba6dbc9438adbcfae6d
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-09-11 16:13:29 -0700

    Create fuel.cpp

---
+++ legacyfiles/fuel.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <math.h>
+class FuelCalc {
+public:
+	float EstFuelCons; //Estimated fuel fonsumption
+	float TTF; //Time til race finish
+	int FIC; //Current fuel in car
+	std::vector<float> Laptime;
+
+	int FinalCalculation() {
+		//empty for now
+	}
+	void LapToSeconds(int minuit, float seconds, std::vector<float> Laptimes) {
+		float result = 0;
+		while (Laptime.size() <= 5) {
+			minuit *= 60;
+			result = minuit + seconds;
+			continue;
+			if (minuit < 1) {
+				break;
+			}
+		}
+	}
+	int AvgCalc(std::vector<float> Laptimes) {
+		double sumOfAll = 0;
+		double average = 0;
+		for (int i = 0; i <= Laptimes.size(); i++) {
+			sumOfAll = +Laptimes[i];
+
+		}
+		average = sumOfAll / Laptimes.size();
+
+		return average;
+	}
+
+	int ProjectedLaps() {
+		int numLaps = 0;
+		int numLaps = TTF / AvgCalc(Laptime);
+		return numLaps;
+	}
+
+	int FuelForStint(int numLaps) {
+		int LitersReq = 0;
+		LitersReq = numLaps * EstFuelCons;
+		return LitersReq;
+	}
+
+
+	void nums(float EstFuelCons, float TTF, int FIC) { //constructor
+		this->EstFuelCons = EstFuelCons;
+		this->TTF = TTF;
+		this->FIC = FIC;
+	}
+
+};
