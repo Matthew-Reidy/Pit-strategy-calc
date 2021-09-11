850b6303735f180512dd30fa2bf44f5e3d50feee
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-09-11 16:15:36 -0700

    Add files via upload
    moved old cpp files to legacy

---
+++ legacyfiles/Main.cpp
@@ -0,0 +1,17 @@
+// ACCpitstopcalc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "Header.h"
+#include <iostream>
+//place holders in main until I can make gui
+int main()
+{
+	
+	/*
+	* sameple contstructor call
+	FuelCalc Strat1 = FuelCalc();
+	FuelCalc.FuelForStint;
+	*/
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
