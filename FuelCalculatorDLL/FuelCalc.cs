f39aade1a80f880654261b6efa53d00e8ce01e79
Shentar1 <90427699+Shentar1@users.noreply.github.com>
2022-11-26 15:21:34 -0600

    Add files via upload

--- FuelCalculatorDLL/FuelCalc.cs
+++ FuelCalculatorDLL/FuelCalc.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +7,35 @@ namespace FuelCalculatorDLL
 {
     public static class FuelCalc
     {
-		static decimal EstFuelCons; //Estimated fuel fonsumption
-		static decimal TTF; //Time til race finish
-		static int FIC; //Current fuel in car
-		static System.Collections.Generic.List<string> LaptimesString = new System.Collections.Generic.List<string>();
-		static List<int> LapTimesNumber = new List<int>();
-
-		public static int FinalCalculation(decimal estFuelCons, int estimatedLaps)
+        /// <summary>
+        /// Calculate how much fuel is needed for minimum and maximum and average laps
+        /// </summary>
+        /// <param name="timeRemainingMilliseconds">session time remaining in ms</param>
+        /// <param name="lapTime">list of lap times set in session</param>
+        /// <param name="fuelPerLap">fuel used per lap in session</param>
+        /// <param name="currentFuel">fuel at time of calculation</param>
+        /// <param name="fuelTankSize">max size of the fuel tank</param>
+        /// <returns>composite list of all fuel data</returns>
+        public static List<decimal> CalculateFuel(decimal timeRemainingMilliseconds, decimal lapTime, decimal fuelPerLap, decimal currentFuel, decimal fuelTankSize)
+        {
+            List<decimal> fuelData = new List<decimal>();
+            decimal estimatedLaps = ProjectedLaps(timeRemainingMilliseconds, lapTime);
+            decimal fillTo = FinalCalculation(fuelPerLap, (int)estimatedLaps, currentFuel);
+            decimal fuelDuration = currentFuel != 0 ?  Math.Floor(currentFuel / fuelPerLap) : Math.Floor(fillTo/fuelPerLap);
+            
+            if (fillTo > fuelTankSize)
+            {
+                fillTo = (int)fuelTankSize;
+            }
+            decimal fuelAtEnd = fillTo + currentFuel - (estimatedLaps * fuelPerLap);
+            fuelData.Add(fuelDuration);
+            fuelData.Add(fillTo);
+			fuelData.Add(fuelAtEnd);
+            return fuelData;
+        }
+        public static int FinalCalculation(decimal estFuelCons, int estimatedLaps, decimal currentFuel)
 		{
-			return (int)Math.Round(estFuelCons * estimatedLaps,0);
+			return (int)Math.Ceiling(estFuelCons * estimatedLaps - currentFuel); //can also use math.floor or ceil to avoid type casting
 		}
 		public static decimal LapToSeconds(decimal minutes, decimal seconds)
 		{
@@ -23,39 +43,12 @@ namespace FuelCalculatorDLL
             decimal result = minutes + seconds;
 			return result;
         }
-		public static decimal AvgCalc(List<decimal> lapTimes)
-		{
-			decimal sumOfAll = 0;
-			decimal average = 0;
-			foreach(decimal lapTime in lapTimes)
-            {
-				sumOfAll += lapTime;
-            }
-			average = sumOfAll / lapTimes.Count;
-
-			return average;
-		}
 
 		public static int ProjectedLaps(decimal timeRemaining, decimal averageLapTime)
 		{
-			int numLaps = 0;
-			numLaps = (int)Math.Round(timeRemaining /averageLapTime,0);
-			return numLaps;
+			int numLaps = averageLapTime != 0 ? (int)Math.Ceiling(timeRemaining /averageLapTime) : 0; //can also use math.floor or ceil to avoid type casting
+			return (int)numLaps;
 		}
 
-		public static int FuelForStint(int numLaps)
-		{
-			int LitersReq = 0;
-			LitersReq = numLaps * (int)Math.Round(EstFuelCons, 0);
-			return LitersReq;
-		}
-
-
-		public static void nums(decimal EstFuelCons, decimal TTF, int FIC)
-		{
-			FuelCalc.EstFuelCons = EstFuelCons;
-			FuelCalc.TTF = TTF;
-			FuelCalc.FIC = FIC;
-		}
 	}
 }
